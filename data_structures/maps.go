0c186735ee4d571ab769dfe10bca4d20c24f649a
Prabhat Jena <prajena@ebay.com>
2016-07-22 17:58:37 +0530

    map-test

--- data_structures/maps.go
+++ data_structures/maps.go
@@ -1,9 +1,21 @@
 package data_structures
 
 func findPeopleWithCommonInterest(data map[string][]string, interest string) []string {
-	return nil
+	res := make([]string, 0)
+	// v, ok := data["adsf"]
+	for k,v := range data { // for each key,val
+		if contains(v, interest) ==true{ // if v contains interest
+			res = append(res, k)
+		}
+	}
+	return res
 }
 
 func contains(src []string, elem string) bool {
+	for s:= range src{
+		if src[s] == elem{
+			return true
+		}
+	}
 	return false
 }
