238c220e24b9dc06167cbacde245060e057f211f
Prabhat Jena <prajena@ebay.com>
2016-07-22 16:13:13 +0530

    for-test

--- control_structures/for_structure.go
+++ control_structures/for_structure.go
@@ -1,5 +1,16 @@
 package control_structures
 
 func collatzChainLength(n int) int {
-	return 0
+	i := 0;
+	for n>1 {
+		
+			if n % 2==0{
+				n = n/2
+			}else{
+				n=3*n+1
+			}
+		
+		i = i+1
+	}
+	return i
 }
