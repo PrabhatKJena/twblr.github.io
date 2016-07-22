fc90d20d12d7c28a5b63f274f8ec008e39b0157f
Prabhat Jena <prajena@ebay.com>
2016-07-22 15:29:00 +0530

    if-test

--- control_structures/if_structure.go
+++ control_structures/if_structure.go
@@ -1,5 +1,16 @@
 package control_structures
 
-func fizzBuzz(i int32) string {
-	return ""
+import (
+    "fmt"
+)
+
+func fizzBuzz(input int32) string {
+	if input % 3 == 0 && input % 5 == 0 {
+		return "FizzBuzz"
+	} else if input % 5 == 0 {
+		return "Buzz";
+	} else if input % 3 == 0 {
+		return "Fizz";
+	}
+	return fmt.Sprintf("%v",input);
 }
