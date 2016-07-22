1da84f0941a9afc05bbe84799e17b49713834b6b
Prabhat Jena <prajena@ebay.com>
2016-07-22 21:15:39 +0530

    Linked list Implementation

--- data_structures/slices.go
+++ data_structures/slices.go
@@ -4,22 +4,52 @@ type mapOperation func(int32) int32
 type filterOperation func(int32) bool
 
 func mapInts(op mapOperation, vals []int32) []int32 {
-	return []int32{}
-
+	for i:= range vals {
+		vals[i] = op(vals[i])
+	}
+	return vals
 }
 
 func filterInts(op filterOperation, vals []int32) []int32 {
-	return []int32{3}
+	a := make([]int32,0)
+	for i:= range vals {
+		if r:= op(vals[i]); r==true{
+			a = append(a, vals[i])
+		}
+	}
+	return a[:]
 }
 
 func concatenate(dest []string, newValues ...string) []string {
-	return nil
+	d := make([]string, len(dest))
+	d = dest[:]
+	for _,s:= range newValues{
+		d = append(d, s)
+	} 
+	return d
 }
 
 func equals(list1 []string, list2 []string) bool {
-	return false
+	l1 := len(list1)
+	l2 := len(list2)
+	if l1 == 0 && l2 == 0{
+		return true
+	}
+	if l1 != l2{
+		return false
+	}
+	for i,s:=range list1{
+		if(s != list2[i]){
+			return false
+		}
+	}
+	return true
 }
 
 func partialReverse(src []int, from, to int) []int {
-	return nil
+	rev := make([]int, 0)
+	for i:=to;i>=from;i--{
+		rev = append(rev, src[i])
+	}
+	return rev
 }
--- data_structures/structs.go
+++ data_structures/structs.go
@@ -2,12 +2,34 @@ package data_structures
 
 // Linked List Implementation
 
-type Node struct{}
+type Node struct{
+	data int
+	next *Node     // Pointer to the next node of the single linked list
+}
+
+var start *Node = nil  	//  Start Pointer to the Linked list
 
 func Add(elem int) {
+	node := Node{elem, nil}   // crate a new node
+	if start == nil{    // if initially the list is empty, then add as a first node
+		start = &node
+	}else{				// else, traverse the list to end then add the new node
+		ptr:=start
+		for ptr.next!=nil{
+			ptr = ptr.next
+		}
+		ptr.next = &node
+	}
 	return
 }
 
-func Search(elem int) *Node {
-	return nil
+func Search(elem int) *Node {   // traverse from start to end and match each node with the required elem
+	ptr := start
+	for ptr!=nil{
+		if ptr.data == elem{  // if found then return elem
+			return ptr
+		} 
+		ptr = ptr.next
+	}
+	return nil   // return nil if not found
 }
